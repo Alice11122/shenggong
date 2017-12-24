421400a0687b4c7e3271a1ee03a31ec86a89f8e2
xyx1 <3327404190@qq.com>
2017-12-24 16:50:14 +0800

    "xyx1提交"

--- src/shengong/sshtest/TestSsh.java
+++ src/shengong/sshtest/TestSsh.java
@@ -1,7 +1,10 @@
 package shengong.sshtest;
-
-public class TestSsh {
+public class TestSsh {
 	public static void main(String[] args) {
+		System.out.println("shengong");
+		System.out.println("shengong");
+		System.out.println("shengong");
+		System.out.println("shengong");
 		System.out.println("shengong");
 	}
 }
